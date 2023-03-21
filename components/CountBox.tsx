523ded3b840198cc9f888026bd4a4db48157f6b6
David Ho <davidho0403@gmail.com>
2023-03-21 22:22:40 +0800

    feat: convert number to locale string

--- components/CountBox.tsx
+++ components/CountBox.tsx
@@ -1,7 +1,7 @@
 const CountBox = (Props: { count: number; type: string; href: string }) => {
   return (
     <a className="flex flex-col items-center bg-white p-3 rounded-3xl w-24 md:w-32" href={Props.href}>
-      <p className="text-xl text-slate-800">{Props.count}</p>
+      <p className="text-xl text-slate-800">{Props.count.toLocaleString()}</p>
       <p className="text-slate-600">{Props.type}</p>
     </a>
   )
