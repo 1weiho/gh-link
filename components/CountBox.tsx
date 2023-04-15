cd81768f918ed652c802453117e74ae123bb1e62
David Ho <davidho0403@gmail.com>
2023-04-16 01:52:50 +0800

    feat: set background white

--- components/CountBox.tsx
+++ components/CountBox.tsx
@@ -1,7 +1,7 @@
 const CountBox = (Props: { count: number; type: string; href: string }) => {
   return (
-    <a className="flex flex-col items-center bg-white p-3 rounded-3xl w-24 md:w-32" href={Props.href}>
     <a
+      className="flex flex-col items-center bg-stone-50 p-3 rounded-3xl w-24 md:w-32"
       href={Props.href}
     >
       <p className="text-xl text-slate-800">{Props.count.toLocaleString()}</p>
