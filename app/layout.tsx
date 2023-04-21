7c95e91cf0bc6b2d935da13492fb38fcdcfc8698
David Ho <davidho0403@gmail.com>
2023-04-21 17:10:33 +0800

    feat: improve metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,29 @@ import { Analytics } from "@vercel/analytics/react";
 export const metadata = {
   title: "GH Link",
   description: "A Link in Bio website configured with only a JSON file",
+  keywords: "link in bio, linktree, linktr.ee, link, bio, link bio, linktree, linktr.ee, linktr, linktree alternative, linktr alternative, linktr.ee alternative",
+  authors: [{ name: "Ho Yi Wei", url: "https://www.github.com/davidho0403" }],
+  openGraph: {
+    type: "website",
+    url: "https://gh-link.vercel.app",
+    title: "GH Link",
+    description: "A Link in Bio website configured with only a JSON file",
+    images: [
+      {
+        url: "https://gh-link.vercel.app/og.png",
+        width: 1200,
+        height: 630,
+        alt: "GH Link",
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'GH Link',
+    description: 'A Link in Bio website configured with only a JSON file',
+    creator: '@1weiho',
+    images: ['https://gh-link.vercel.app/og.png'],
+  },
 };
 
 export default function RootLayout({
