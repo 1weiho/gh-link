a07033416bb5d604e389e542727fe959c44e7c4d
David Ho <davidho0403@gmail.com>
2024-02-03 15:58:42 +0800

    fix: add custom tailwind animate back to config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,10 +67,22 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        scrollDown: {
+          "0%": { transform: "translate(0%, 0%)" },
+          "50%": { transform: "translate(0%, -15%)" },
+          "100%": { transform: "translate(0%, 0%)" },
+        },
+        clipboard: {
+          "0%": { transform: "scale(1)", color: "rgb(100 116 139)" },
+          "25%": { transform: "scale(0.8)" },
+          "50%": { transform: "scale(1)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        scrollDown: "scrollDown 2s ease-in-out infinite",
+        clipboard: "clipboard 0.4s ease-in-out forwards",
       },
     },
   },
